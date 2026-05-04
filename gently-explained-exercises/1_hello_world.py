c214ede24c5b240ade0be1369e4bdf255fc99fa4
m-radk0 <mitovradostin@gmail.com>
2026-05-04 17:31:42 +0300

    Added a basic print exercise

---
+++ gently-explained-exercises/1_hello_world.py
@@ -0,0 +1,9 @@
+"""
+Write a program that, when run, greets the user by printing ―Hello, world!‖ on the screen. Then it
+prints a message on the screen asking the user to enter their name. The program greets the user by
+name by printing the ―Hello,‖ followed by the users name.
+"""
+
+print('Hello, world!')
+name = input('What is your name?\n')
+print(f"Hello, {name}")
