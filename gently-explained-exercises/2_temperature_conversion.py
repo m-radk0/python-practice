be02d09d8f0d0cde6723911755c6186afd8acbe6
m-radk0 <mitovradostin@gmail.com>
2026-05-05 09:42:08 +0300

    Added a solution

---
+++ gently-explained-exercises/2_temperature_conversion.py
@@ -0,0 +1,11 @@
+def convertToFahrenheit(degreesCelsius):
+    return degreesCelsius * (9 / 5) + 32
+    
+def convertToCelsius(degreesFahrenheit):
+    return (degreesFahrenheit - 32) * (5 / 9)
+    
+assert convertToCelsius(0) == -17.77777777777778
+assert convertToCelsius(180) == 82.22222222222223
+assert convertToFahrenheit(0) == 32
+assert convertToFahrenheit(100) == 212
+assert convertToCelsius(convertToFahrenheit(15)) == 15
